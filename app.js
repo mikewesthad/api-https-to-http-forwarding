f2687eb055a02bfbfa73e026a5c95d884d5263f0
Michael Hadley <michaelwesthadley@gmail.com>
2016-06-22 21:53:28 -0500

    Adding comments

--- app.js
+++ app.js
@@ -1,22 +1,29 @@
 const express = require("express");
 const cors = require("cors");
 const request = require("request");
-
 const app = express();
 
-// Cross-origin is necessary in order for node to accept requests 
-const corsOptions = {
+// Enable CORS middleware on all routes
+// This isn't a real security measure since origin can be spoofed, but it's a 
+// step. Configure this to whitelist the origins you approve.
+app.use(cors({
     origin: "https://www.mikewesthad.com"
-};
+}));
 
-app.use(cors(corsOptions));
-
+// GET requests to the root will:
+//  - Get the "url" query parameter from the request
+//  - Make a request to the url and stream it back to the client
+// The client should make sure its query values are URI encoded:
+//  - /?url=http%3A%2F%2Fwww.google.com
+//  - /?url=http%3A%2F%2Fjsonplaceholder.typicode.com%2Fcomments%3FpostId%3D1
 app.get("/", function (req, res, next) {
+    // Check for "url" query parameter
     if (!req.query.url) {
         res.status(404).send("Bad url in request");
         next();
     }
-
+    // Decode the "url" query value and open a request that points to that
+    // location
     const apiUrl = decodeURIComponent(req.query.url);
     request(apiUrl)
         .on("error", function (error) {
@@ -26,6 +33,7 @@ app.get("/", function (req, res, next) {
         .pipe(res);
 });
 
+// Fallback for all other routes
 app.all("*", function (req, res) {
     res.status(404).send("Error, unhandled route!");
 });
