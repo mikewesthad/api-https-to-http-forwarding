969dca2285406639500ba1bfddf271437e37959a
Michael Hadley <michaelwesthadley@gmail.com>
2016-06-22 20:06:52 -0500

    Add basic forwarding app

---
+++ app.js
@@ -0,0 +1,31 @@
+const express = require("express");
+const cors = require("cors");
+const request = require("request");
+
+const app = express();
+
+const corsOptions = {
+    origin: "https://www.mikewesthad.com/"
+};
+
+app.get("/", cors(corsOptions), function (req, res, next) {
+    if (!req.query.url) {
+        res.status(404).send("Bad url in request");
+        next();
+    }
+
+    const apiUrl = decodeURIComponent(req.query.url);
+    request(apiUrl)
+        .on("error", function (error) {
+            res.status(404).send("Error in making API request");
+            next();
+        })
+        .pipe(res);
+});
+
+app.all("*", function (req, res) {
+    res.status(404).send("Error, unhandled route!");
+});
+
+var port = process.env.PORT || 8080;
+app.listen(port);
